45b144f5131ee79ee0c32c18d317ebd2cd19837e
fall211 <tuukka02@gmail.com>
2022-04-10 14:56:59 -0700

    added run fn and first receptors

--- level.py
+++ level.py
@@ -1,7 +1,22 @@
+import pygame
+from receptor import Receptor
+
 class Level:
     def __init__(self, lvl_num):
-        stage_num = lvl_num
+        self.stage_num = lvl_num
+        self.surface = pygame.display.get_surface()
+        self.receptorgroup = pygame.sprite.Group()
+
+        receptorposlist = [(192,541),(279,541),(366,541),(453,541)]
+
+        for pos in receptorposlist:
+            Receptor(pos,self.receptorgroup)
+
+
     def load(self):
         x = 4
+
+    def run(self):
+        self.receptorgroup.draw(self.surface)
 
 #TODO make a basic level class

4c7693913fa15f14307fbf52284fcfcb06918109
fall211 <tuukka02@gmail.com>
2022-04-10 15:19:55 -0700

    added comments for clarity

--- level.py
+++ level.py
@@ -4,19 +4,19 @@ from receptor import Receptor
 class Level:
     def __init__(self, lvl_num):
         self.stage_num = lvl_num
-        self.surface = pygame.display.get_surface()
-        self.receptorgroup = pygame.sprite.Group()
+        self.surface = pygame.display.get_surface() #gets the surface so that we can draw on it
+        self.receptorgroup = pygame.sprite.Group() #sprite group for the receptors
 
-        receptorposlist = [(192,541),(279,541),(366,541),(453,541)]
+        receptorposlist = [(192,541),(279,541),(366,541),(453,541)] #just a list with positions
 
         for pos in receptorposlist:
-            Receptor(pos,self.receptorgroup)
+            Receptor(pos,self.receptorgroup) #creates instances of the receptor class and adds them to its sprite group
 
 
     def load(self):
         x = 4
 
-    def run(self):
+    def run(self): #this is the thing that gets ran every time the screen updates, so this will be used to draw all the shit
         self.receptorgroup.draw(self.surface)
 
 #TODO make a basic level class
--- receptor.py
+++ receptor.py
@@ -2,8 +2,8 @@ import pygame
 
 class Receptor(pygame.sprite.Sprite):
 	def __init__(self,pos,group):
-		super().__init__(group)
+		super().__init__(group) # we need this, not quite sure why but it's here
 
 		self.pos = pos
 		self.image = pygame.image.load('images/receptor.png').convert()
-		self.rect = self.image.get_rect(topleft=self.pos)
+		self.rect = self.image.get_rect(topleft=self.pos) #rectangle is for positioning the sprite and also later for collisions

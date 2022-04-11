bf1cda653c8037a21ee95195913f841c1d38729e
gskapoor <kapoorgobind@gmail.com>
2022-04-10 21:19:56 -0700

    added some time support

--- level.py
+++ level.py
@@ -4,6 +4,10 @@ from note import Note
 
 class Level:
     def __init__(self, lvl_num):
+        self.stage_clock = pygame.time.Clock()
+        self.dt = self.stage_clock.tick()
+        self.te = 0
+
         self.stage_num = lvl_num
         self.surface = pygame.display.get_surface() #gets the surface so that we can draw on it
         self.receptorgroup = pygame.sprite.Group() #sprite group for the receptors
@@ -12,17 +16,26 @@ class Level:
         self.background = pygame.image.load("images/background.png")
 
         receptorposlist = [(192,541),(279,541),(366,541),(453,541)] #just a list with positions
-        notearr = [(0, 100), (1, 200), (2, 300), (3, 400)]
+        self.notearr = [(100, (0, 341)), (200, (0, 341))]
+        self.onPos = 0;
 
         for pos in receptorposlist:
             Receptor(pos,self.receptorgroup) #creates instances of the receptor class and adds them to its sprite group
-        for pos in notearr:
-            Note(pos, self.notegroup)
 
     def run(self): #this is the thing that gets ran every time the screen updates, so this will be used to draw all the shit
+        while(self.onPos < len(self.notearr) and self.notearr[self.onPos][0] - self.te < 0.03):
+            Note(self.notearr[self.onPos][1], self.notegroup)
+            self.onPos += 1
+        
+        print(self.te)
+
+        self.stage_clock.tick()
         self.surface.blit(self.background, (0, 0))
         self.receptorgroup.draw(self.surface)
         self.notegroup.draw(self.surface)
         self.notegroup.update()
+
+        self.dt = self.stage_clock.tick()
+        self.te += self.dt
 
 #TODO make a basic level class

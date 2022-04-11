088ca4fb32cdf0a06ba24d913864f1db441532f2
gskapoor <kapoorgobind@gmail.com>
2022-04-10 19:14:47 -0700

    fix

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import game
-=======
 from game import Game
-from sys import exit
->>>>>>> 30ae85179a59dbb89624955d407e0f4ca85678c6
 
 
 #https://www.pygame.org/docs/   <---- quick access to documentation
